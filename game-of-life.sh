abb12ab2e7a75f8897ed67ac32e9618cd76a004e
medianet <medianet@ride.lan>
2024-01-12 00:15:37 +0100

    also start LED stuff...

--- game-of-life.sh
+++ game-of-life.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+for i in mn-unicorn-1 mn-unicorn-2 ; do
+	ssh -A medianet@$i "\
+		killall -KILL python3.11 ;\
+		/usr/bin/python3.11 /home/medianet/no-mans-sky/gol-unicorn.py &" \
+	&
+done
+for i in mn-sense-1 ; do
+	ssh -A medianet@$i "\
+		killall -KILL python3.11 ;\
+		/usr/bin/python3.11 /home/medianet/no-mans-sky/gol-sense.py &" \
+	&
+done
 for i in ride jemison lovelace mn-amp-1 mn-amp-2 mn-sense-1 mn-unicorn-1 mn-satellite mn-node-3; do 
 	ssh -A medianet@$i "\
 		killall -KILL mpv ;\
